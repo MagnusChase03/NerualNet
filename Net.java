4d356b5ac6fe1356766b7a4675645b8c0c764505
Zachary <42364001+MagnusChase03@users.noreply.github.com>
2019-10-20 20:41:19 -0500

    Update Net.java

--- Net.java
+++ Net.java
@@ -1,45 +1,25 @@
 public class Net {
 
-    public static void main(String[] args) {
+    private double[][] input;
+    private double[][] output;
+    private double[] weights;
     
-        double[][] input = {{0, 0, 1},
-                            {1, 1, 1},
-                            {1, 0, 1},
-                            {0, 1, 1}};
-                            
-        double[][] output = {{0, 1, 1, 0}};
-        output = Math2.transpose(output);
+    public Net(double[][] i, double[][] o, double[] w) {
     
-        double[] weights = {(Math.random() * 2) - 1, 
-                            (Math.random() * 2) - 1, 
-                            (Math.random() * 2) - 1};
+        input = i;
+        output = o;
+        weights = w;
+    
+    }
+    
+    public void train(int times) {
+    
+        for (int i = 0; i < times; i++) {
         
-        for (int i = 0; i < 10000; i++) {
-        
-            double[] guess = Math2.dot(input, weights);
-            for (int l = 0; l < guess.length; l++) {
-            
-                guess[l] = Math2.sigmoid(guess[l]);
-            
-            }
-            
-            double[] error = new double[guess.length];
-            for (int l = 0; l < error.length; l++) {
-            
-                error[l] = output[l][0] - guess[l];
-            
-            }
-            
-            double[] change = new double[error.length];
-            
-            double[] dSigGuess = new double[guess.length];
-            for (int k = 0; k < dSigGuess.length; k++) {
-            
-                dSigGuess[k] = Math2.dSigmoid(guess[k]) * error[k];
-            
-            }
-            
-            change = Math2.dot(Math2.transpose(input), dSigGuess);
+            double[] guess = getGuess();
+            double[] error = getError(guess);
+            double[] dSigGuess = getDSigmoidError(guess, error);
+            double[] change = Math2.dot(Math2.transpose(input), dSigGuess);
             
             for (int l = 0; l < weights.length; l++) {
             
@@ -47,16 +27,95 @@ public class Net {
             
             }
         
-        }    
-        
-        double[] guess = Math2.dot(new double[][] {{1, 0, 0}}, weights);
+        }
+    
+    }
+    
+    public double[] getGuess() {
+    
+        double[] guess = Math2.dot(input, weights);
         for (int l = 0; l < guess.length; l++) {
         
             guess[l] = Math2.sigmoid(guess[l]);
         
         }
         
-        System.out.println(guess[0]);
+        return guess;
+    
+    }
+    
+    public double[] getError(double[] guess) {
+    
+        double[] error = new double[guess.length];
+        for (int l = 0; l < error.length; l++) {
+        
+            error[l] = output[l][0] - guess[l];
+        
+        }
+        
+        return error;
+    
+    }
+    
+    public double[] getDSigmoidError(double[] guess, double[] error) {
+    
+        double[] dSigGuess = new double[guess.length];
+        for (int k = 0; k < dSigGuess.length; k++) {
+        
+            dSigGuess[k] = Math2.dSigmoid(guess[k]) * error[k];
+        
+        }
+        
+        return dSigGuess;
+    
+    }
+    
+    public double[] testInputs(double[][] input) {
+    
+        double[] guess = Math2.dot(input, weights);
+        for (int i = 0; i < guess.length; i++) {
+        
+            guess[i] = Math2.sigmoid(guess[i]);
+        
+        }
+        
+        return guess;
+    
+    }
+
+    public double[] getWeights() {
+    
+        return weights;
+    
+    }
+
+    public static void main(String[] args) {
+    
+        double[][] input = {{0, 0, 1},
+                            {1, 1, 1},
+                            {1, 0, 1},
+                            {0, 1, 1},
+                            {0, 0, 0},
+                            {1, 0, 0}};
+                            
+        double[][] output = {{0, 1, 0, 1, 0, 0}};
+        output = Math2.transpose(output);
+    
+        double[] weights = {(Math.random() * 2) - 1, 
+                            (Math.random() * 2) - 1, 
+                            (Math.random() * 2) - 1};
+        
+        Net net = new Net(input, output, weights);
+        net.train(60000);  
+        
+        double[] test = net.testInputs(new double[][] {{1, 1, 0}});
+        System.out.println(test[0]);  
+        
+        for (double w : net.getWeights()) {
+        
+            System.out.println(w);
+        
+        }
         
     }
 
